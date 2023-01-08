3ec4b0ea815f2fdcedabe8c64a6ae3cb0c5ac7e0
max.shatilo@gmail.com <max.shatilo@gmail.com>
2023-01-08 22:34:21 +0200

    Add find prime numbers functionality implementation

---
+++ FindPrimeNumbers/PrimeNumbersHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Concurrent;
+
+namespace FindPrimeNumbers
+{
+    public static class PrimeNumbersHelper
+    {
+        public static IList<int> GetPrimeListUsingThreads(int start, int end)
+        {
+            var primeNumbers = new List<int>();
+
+            var range = end - start;
+            var numberOfThreads = Environment.ProcessorCount;
+
+            var threads = new Thread[numberOfThreads];
+            var chunkSize = range / numberOfThreads;
+
+            var lockObject = new object();
+
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                var chunkStart = start + i * chunkSize;
+                var chunkEnd = i == (numberOfThreads - 1) ? end : chunkStart + chunkSize;
+
+                threads[i] = new Thread(() =>
+                {
+                    for (var number = chunkStart; number < chunkEnd; ++number)
+                    {
+                        if (IsPrime(number))
+                        {
+                            lock (lockObject)
+                            {
+                                primeNumbers.Add(number);
+                            }
+                        }
+                    }
+                });
+
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return primeNumbers;
+        }
+
+        public static async Task<IList<int>> GetPrimeListAsync(IList<int> numbers)
+        {
+            return await Task.Run(() => numbers.Where(IsPrime).ToList());
+        }
+
+        public static IList<int> GetPrimeListWithParallel(IList<int> numbers)
+        {
+            var primeNumbers = new ConcurrentBag<int>();
+
+            Parallel.ForEach(numbers, number =>
+            {
+                if (IsPrime(number))
+                {
+                    primeNumbers.Add(number);
+                }
+            });
+
+            return primeNumbers.ToList();
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- FindPrimeNumbers/Program.cs
+++ FindPrimeNumbers/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FindPrimeNumbers;
+using System.Diagnostics;
+
+string? userEnteredString = string.Empty;
+int enteredIntegerNumber = 0;
+bool isInteger = false;
+bool isEnteredIntegerAboveTwo = false;
+
+while (string.IsNullOrWhiteSpace(userEnteredString) || !isInteger || !isEnteredIntegerAboveTwo)
+{
+    Console.Write("Please, enter any integer above 2 up to render the prime numbers from 0 to your entered number:");
+    userEnteredString = Console.ReadLine();
+    isInteger = int.TryParse(userEnteredString, out enteredIntegerNumber);
+
+    if (isInteger)
+    {
+        isEnteredIntegerAboveTwo = enteredIntegerNumber > 2;
+
+        if (!isEnteredIntegerAboveTwo)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"You've entered: {enteredIntegerNumber} number, which is less or equal 2, please try again (!)");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"You've entered: {userEnteredString} , which is not a integer number, please try again (!)");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+var limit = enteredIntegerNumber;
+var numbers = Enumerable.Range(0, limit).ToList();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Find below please your results - found prime numbers from 0 to {enteredIntegerNumber}:");
+Console.ForegroundColor = ConsoleColor.Green;
+
+//Threads way of the implementation execution
+var watchForThreads = Stopwatch.StartNew();
+var primeNumbersFromThreads = PrimeNumbersHelper.GetPrimeListUsingThreads(0, limit);
+watchForThreads.Stop();
+Console.WriteLine($"Prime numbers returned by the 'Threads' way of the implementation execution: {string.Join(", ", primeNumbersFromThreads.Order())}");
+Console.WriteLine($"Time spent for the 'Threads' way of the implementation execution: {watchForThreads.ElapsedMilliseconds} ms");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("------------------------------------------------------------------------");
+Console.ForegroundColor = ConsoleColor.Green;
+
+//Task way of the implementation execution
+var watchForTask = Stopwatch.StartNew();
+var primeNumbersFromTask = PrimeNumbersHelper.GetPrimeListAsync(numbers);
+watchForTask.Stop();
+Console.WriteLine($"Prime numbers returned by the 'Task' way of the implementation execution: {string.Join(", ", (await primeNumbersFromTask).Order())}");
+Console.WriteLine($"Time spent for the 'Task' way of the implementation execution: {watchForTask.ElapsedMilliseconds} ms");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("------------------------------------------------------------------------");
+Console.ForegroundColor = ConsoleColor.Green;
+
+//Parallel.ForEach way of the implementation execution
+var watchForParallel = Stopwatch.StartNew();
+var primeNumbersFromParallelForeach = PrimeNumbersHelper.GetPrimeListWithParallel(numbers);
+watchForParallel.Stop();
+Console.WriteLine($"Prime numbers returned by the 'Parallel.ForEach' way of the implementation execution: {string.Join(", ", primeNumbersFromParallelForeach.Order())}");
+Console.WriteLine($"Time spent for the 'Parallel.ForEach' way of the implementation execution: {watchForParallel.ElapsedMilliseconds} ms");
+
+Console.WriteLine(String.Empty);
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
